72ce0b748894d0510392b6282617cf4b61a3c462
Yann Prono <yann.prono@telecomnancy.net>
2025-04-20 13:13:30 +0200

    new deployment

---
+++ slides/2025-04-17-devoxxfr-ratatui/assets/md-Dht32xpH.js
@@ -0,0 +1,15 @@
+import{_}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-D1VBF_4v.js";import{aJ as c,b as u,o as e,w as a,g as s,ac as n,ag as f,f as m,e as d,m as J,v as g,x as k,C as i}from"./modules/vue-CcxHahpj.js";import{_ as v}from"./slidev/two-cols.vue_vue_type_script_setup_true_lang-CBNoV_HF.js";import{u as h,f as x}from"./slidev/context-tjl6qq8u.js";import"./modules/unplugin-icons-Co_e7TWu.js";import"./index-Cj14mpOl.js";import"./modules/shiki-DfFjDED3.js";const D={__name:"slides.md__slidev_8",setup(T){const{$clicksContext:t,$frontmatter:r}=h();return t.setup(),(U,l)=>{const p=_,o=c("click");return e(),u(v,g(k(i(x)(i(r),7))),{right:a(B=>[f((e(),m("div",null,[d(p,J({lines:!1},{ranges:["*"]}),{default:a(()=>l[0]||(l[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"                 ___")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  ___======____=---=)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"/T            \\_--===)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"[ \\ (O)   \\~    \\_-==)")]),n(`
+`),s("span",{class:"line"},[s("span",null," \\      / )J~~    \\-=)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  \\\\___/  )JJ~~~   \\)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   \\_____/JJJ~~~~    \\")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   / \\  , \\J~~~~~     \\")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  (-\\)\\=|\\\\\\~~~~       L__")]),n(`
+`),s("span",{class:"line"},[s("span",null,"  (\\\\)  (\\\\\\)_           \\==__")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   \\V    \\\\\\) ===_____   \\\\\\\\\\\\")]),n(`
+`),s("span",{class:"line"},[s("span",null,"          \\V)     \\_) \\\\\\\\JJ\\J\\)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                      /J\\JT\\JJJJ)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                      (JJJ| \\UUU)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"                       (UU)")])])],-1)])),_:1},16),l[1]||(l[1]=s("figcaption",null,"Fish shell logo",-1))])),[[o]])]),default:a(()=>[l[2]||(l[2]=s("h1",null,[s("strong",null,"T"),n("erminal "),s("strong",null,"U"),n("ser "),s("strong",null,"I"),n("nterface")],-1)),l[3]||(l[3]=s("p",null,"Terminal as a canvas for rendering user interfaces",-1))]),_:1},16)}}};export{D as default};
